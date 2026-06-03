ad04d4f6c01ca49522ea9c990178f45ab90188e5
8-321 <huang@an520.xin>
2026-06-03 17:03:17 +0800

    init: 桌面书签 - 折叠态玻璃流动书签，待办/想法/知识/报告，5套主题，开机自启

---
+++ RUN-DesktopBookmark.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "D:\Hanako\desktop-bookmark\dist\DesktopBookmark-portable-fixed"
+start "DesktopBookmark" "D:\Hanako\desktop-bookmark\dist\DesktopBookmark-portable-fixed\DesktopBookmark.exe"
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d D:\Hanako\desktop-bookmark
+start "桌面书签" D:\Hanako\desktop-bookmark\node_modules\electron\dist\electron.exe .
